b3debc44a7f21c41df4fa1d867fa4684a74d60fe
Aravindanm577 <aravindan31032005@gmail.com>
2024-10-04 17:05:19 +0100

    Add files via upload

---
+++ main2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class main2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        String name=sc.nextLine();
+        System.out.println(age);
+        System.out.println(name);
+
+    }
+ }
